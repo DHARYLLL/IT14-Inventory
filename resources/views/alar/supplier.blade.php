2f894940f0d2ef534679fdf3fd2bcce943c1dcd8
flores <garflores16@gmail.com>
2025-10-15 21:20:04 +0800

    minor chnages

--- resources/views/alar/supplier.blade.php
+++ resources/views/alar/supplier.blade.php
@@ -2,161 +2,156 @@
 @section('title', 'Supplier')
 
 @section('content')
-@section('head', 'Supplier')
-@section('name', 'Staff')
+    @section('head', 'Supplier')
+    @section('name', 'Staff')
 
-<div class="d-flex align-items-center justify-content-end p-2 mb-0">
-    <button class="btn btn-green d-flex align-items-center gap-2" type="button" data-bs-toggle="modal"
-        data-bs-target="#NewSupplierModal"><i class="bi bi-plus-lg"></i><span>Add Supplier</span>
-    </button>
-</div>
+    <div class="d-flex align-items-center justify-content-end p-0 cust-h-heading">
+        <button class="btn btn-green d-flex align-items-center gap-2" type="button" data-bs-toggle="modal"
+            data-bs-target="#NewSupplierModal"><i class="bi bi-plus-lg"></i><span>Add Supplier</span>
+        </button>
+    </div>
 
-{{-- table --}}
-<table class="table table-hover align-middle mb-0">
-    <thead>
-        <tr class="table-light">
-            <th class="fw-semibold">Supplier #</th>
-            <th class="fw-semibold">Name</th>
-            <th class="fw-semibold">Contact number</th>
-            <th class="fw-semibold">Company name</th>
-            <th class="fw-semibold">Company Address</th>
-            <th class="fw-semibold text-center">Action</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        @if ($suppliers->isEmpty())
-            <tr>
-                <td colspan="6" class="text-center text-secondary py-3">No New Suppliers.</td>
-            </tr>
-        @else
-            @foreach ($suppliers as $supplier)
-                <tr>
-                    <td>{{ $supplier->id }}</td>
-                    <td>{{ $supplier->fname . ' ' . ($supplier->mname ? $supplier->mname . ' ' : '') . $supplier->lname }}
-                    </td>
-                    <td>{{ $supplier->contact_number }}</td>
-                    <td>{{ $supplier->company_name }}</td>
-                    <td>{{ $supplier->company_address }}</td>
-                    <td class="align-middle">
-                        <div class="d-flex justify-content-center align-items-center gap-2">
-                            <button type="button" class="btn btn-outline-success btn-md" data-bs-toggle="modal"
-                                data-bs-target="#EditSupplierModal{{ $supplier->id }}">
-                                <i class="bi bi-pencil-square" data-bs-toggle="tooltip" data-bs-placement="top"
-                                    title="Edit"></i>
-                            </button>
-
-                            <form action="{{ route('supplier.destroy', $supplier->id) }}" method="POST" class="m-0">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-outline-danger btn-md">
-                                    <i class="bi bi-trash" data-bs-toggle="tooltip" data-bs-placement="top"
-                                        title="Delete"></i>
-                                </button>
-                            </form>
-                        </div>
-                    </td>
+    {{-- table --}}
+    <div class="cust-h-content">
+        <table class="table table-hover align-middle mb-0">
+            <thead>
+                <tr class="table-light">
+                    <th class="fw-semibold">Supplier #</th>
+                    <th class="fw-semibold">Name</th>
+                    <th class="fw-semibold">Contact number</th>
+                    <th class="fw-semibold">Company name</th>
+                    <th class="fw-semibold">Company Address</th>
+                    <th class="fw-semibold text-center">Action</th>
                 </tr>
-
-                <!-- Edit Modal -->
-                <div class="modal fade" id="EditSupplierModal{{ $supplier->id }}" tabindex="-1"
-                    aria-labelledby="EditSupplierModalLabel{{ $supplier->id }}" aria-hidden="true">
-                    <div class="modal-dialog modal-custom">
-                        <div class="modal-content">
-                            {{-- Header --}}
-                            <div class="modal-header modal-header-custom">
-                                <h1 class="modal-title fs-5 m-0" id="EditSupplierModalLabel{{ $supplier->id }}">
-                                    Edit Supplier
-                                </h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                    aria-label="Close"></button>
-                            </div>
-
-                            {{-- Body --}}
-                            <form action="{{ route('supplier.update', ['supplier' => $supplier->id]) }}"
-                                method="POST">
-                                @csrf
-                                @method('PUT')
-                                <div class="modal-body py-3">
-                                    <div class="row g-3">
-                                        <div class="col-md-4">
-                                            <label class="form-label">First Name</label>
-                                            <input type="text" name="fname" class="form-control"
-                                                value="{{ $supplier->fname }}">
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label class="form-label">Middle Name</label>
-                                            <input type="text" name="mname" class="form-control"
-                                                value="{{ $supplier->mname }}">
-                                        </div>
-                                        <div class="col-md-4">
-                                            <label class="form-label">Last Name</label>
-                                            <input type="text" name="lname" class="form-control"
-                                                value="{{ $supplier->lname }}">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label class="form-label">Contact Number</label>
-                                            <input type="text" name="contact_number" class="form-control"
-                                                value="{{ $supplier->contact_number }}">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label class="form-label">Company Name</label>
-                                            <input type="text" name="company_name" class="form-control"
-                                                value="{{ $supplier->company_name }}">
-                                        </div>
-                                        <div class="col-12">
-                                            <label class="form-label">Company Address</label>
-                                            <input type="text" name="company_address" class="form-control"
-                                                value="{{ $supplier->company_address }}">
-                                        </div>
+            </thead>
+            <tbody>
+                @if ($suppliers->isEmpty())
+                    <tr>
+                        <td colspan="6" class="text-center text-secondary py-3">No New Suppliers.</td>
+                    </tr>
+                @else
+                    @foreach ($suppliers as $supplier)
+                        <tr>
+                            <td>{{ $supplier->id }}</td>
+                            <td>{{ $supplier->fname . ' ' . ($supplier->mname ? $supplier->mname . ' ' : '') . $supplier->lname }}
+                            </td>
+                            <td>{{ $supplier->contact_number }}</td>
+                            <td>{{ $supplier->company_name }}</td>
+                            <td>{{ $supplier->company_address }}</td>
+                            <td class="align-middle">
+                                <div class="d-flex justify-content-center align-items-center gap-2">
+                                    <button type="button" class="btn btn-outline-success btn-md" data-bs-toggle="modal"
+                                        data-bs-target="#EditSupplierModal{{ $supplier->id }}">
+                                        <i class="bi bi-pencil-square" data-bs-toggle="tooltip" data-bs-placement="top"
+                                            title="Edit"></i>
+                                    </button>
+                                    <form action="{{ route('supplier.destroy', $supplier->id) }}" method="POST" class="m-0">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-outline-danger btn-md">
+                                            <i class="bi bi-trash" data-bs-toggle="tooltip" data-bs-placement="top"
+                                                title="Delete"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        <!-- Edit Modal -->
+                        <div class="modal fade" id="EditSupplierModal{{ $supplier->id }}" tabindex="-1"
+                            aria-labelledby="EditSupplierModalLabel{{ $supplier->id }}" aria-hidden="true">
+                            <div class="modal-dialog modal-custom">
+                                <div class="modal-content">
+                                    {{-- Header --}}
+                                    <div class="modal-header modal-header-custom">
+                                        <h1 class="modal-title fs-5 m-0" id="EditSupplierModalLabel{{ $supplier->id }}">
+                                            Edit Supplier
+                                        </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
                                     </div>
+                                    {{-- Body --}}
+                                    <form action="{{ route('supplier.update', ['supplier' => $supplier->id]) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="modal-body py-3">
+                                            <div class="row g-3">
+                                                <div class="col-md-4">
+                                                    <label class="form-label">First Name</label>
+                                                    <input type="text" name="fname" class="form-control"
+                                                        value="{{ $supplier->fname }}">
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label class="form-label">Middle Name</label>
+                                                    <input type="text" name="mname" class="form-control"
+                                                        value="{{ $supplier->mname }}">
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <label class="form-label">Last Name</label>
+                                                    <input type="text" name="lname" class="form-control"
+                                                        value="{{ $supplier->lname }}">
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label class="form-label">Contact Number</label>
+                                                    <input type="text" name="contact_number" class="form-control"
+                                                        value="{{ $supplier->contact_number }}">
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <label class="form-label">Company Name</label>
+                                                    <input type="text" name="company_name" class="form-control"
+                                                        value="{{ $supplier->company_name }}">
+                                                </div>
+                                                <div class="col-12">
+                                                    <label class="form-label">Company Address</label>
+                                                    <input type="text" name="company_address" class="form-control"
+                                                        value="{{ $supplier->company_address }}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {{-- Footer --}}
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal"><i
+                                                    class="bi bi-x-lg px-2"></i>Cancel</button>
+                                            <button type="submit" class="btn btn-green"><i class="bi bi-floppy px-2"></i>Save
+                                                Changes</button>
+                                        </div>
+                                    </form>
                                 </div>
-
-                                {{-- Footer --}}
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal"><i
-                                            class="bi bi-x-lg px-2"></i>Cancel</button>
-                                    <button type="submit" class="btn btn-green"><i class="bi bi-floppy px-2"></i>Save
-                                        Changes</button>
-                                </div>
-                            </form>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            @endforeach
-        @endif
-    </tbody>
-</table>
+                    @endforeach
+                @endif
+            </tbody>
+        </table>
+        {{-- Custom Pagination --}}
+        <div class="d-flex justify-content-center mt-3">
+            <nav aria-label="Page navigation example">
+                <ul class="pagination mb-0">
+                    <li class="page-item {{ $suppliers->onFirstPage() ? 'disabled' : '' }}">
+                        <a class="page-link" href="{{ $suppliers->previousPageUrl() ?? '#' }}" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    @for ($i = 1; $i <= $suppliers->lastPage(); $i++)
+                        <li class="page-item {{ $suppliers->currentPage() == $i ? 'active' : '' }}">
+                            <a class="page-link" href="{{ $suppliers->url($i) }}">{{ $i }}</a>
+                        </li>
+                    @endfor
+                    <li class="page-item {{ $suppliers->currentPage() == $suppliers->lastPage() ? 'disabled' : '' }}">
+                        <a class="page-link" href="{{ $suppliers->nextPageUrl() ?? '#' }}" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        {{-- Showing results text --}}
+        <div class="text-center text-secondary mt-2">
+            Showing {{ $suppliers->firstItem() ?? 0 }} to {{ $suppliers->lastItem() ?? 0 }} of
+            {{ $suppliers->total() ?? 0 }} results
+        </div>
+    </div>
 
-{{-- Custom Pagination --}}
-<div class="d-flex justify-content-center mt-3">
-    <nav aria-label="Page navigation example">
-        <ul class="pagination mb-0">
-            <li class="page-item {{ $suppliers->onFirstPage() ? 'disabled' : '' }}">
-                <a class="page-link" href="{{ $suppliers->previousPageUrl() ?? '#' }}" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                </a>
-            </li>
-            @for ($i = 1; $i <= $suppliers->lastPage(); $i++)
-                <li class="page-item {{ $suppliers->currentPage() == $i ? 'active' : '' }}">
-                    <a class="page-link" href="{{ $suppliers->url($i) }}">{{ $i }}</a>
-                </li>
-            @endfor
-            <li class="page-item {{ $suppliers->currentPage() == $suppliers->lastPage() ? 'disabled' : '' }}">
-                <a class="page-link" href="{{ $suppliers->nextPageUrl() ?? '#' }}" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
-</div>
-
-{{-- Showing results text --}}
-<div class="text-center text-secondary mt-2">
-    Showing {{ $suppliers->firstItem() ?? 0 }} to {{ $suppliers->lastItem() ?? 0 }} of
-    {{ $suppliers->total() ?? 0 }} results
-</div>
-</div>
 @endsection
 
 <!-- Add Modal -->
